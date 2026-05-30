65b1e15a60d8b1a2963655e48638c234ac0f703e
Jehosua Carvajal <jehosua.carvajal@gmail.com>
2026-05-30 17:13:03 -0600

    Updates

--- _astro/servicios.astro_astro_type_script_index_0_lang.B_Hjta5S.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./animations.HzcrWY1s.js";a("[data-cards-grid]","[data-card]");
---
+++ _astro/servicios.astro_astro_type_script_index_0_lang.CKkG7KUg.js
@@ -0,0 +1,1 @@
+import{i as a}from"./animations.Be7zWlEG.js";a("[data-cards-grid]","[data-card]");
